8a7f5d816936b4e35127cb395915e848a65d3976
Hai Bison <haibison.apps@gmail.com>
2018-07-11 15:50:18 +0700

    Tests

--- tests/storage.rs
+++ tests/storage.rs
@@ -6,12 +6,12 @@ use binn_ir::storage;
 
 #[test]
 fn storages() {
-    assert!(storage::NO_BYTES   == 0b_000);
-    assert!(storage::BYTE       == 0b_001);
-    assert!(storage::WORD       == 0b_010);
-    assert!(storage::DWORD      == 0b_011);
-    assert!(storage::QWORD      == 0b_100);
-    assert!(storage::STRING     == 0b_101);
-    assert!(storage::BLOB       == 0b_110);
-    assert!(storage::CONTAINER  == 0b_111);
+    assert_eq!(storage::NO_BYTES,   0b_000);
+    assert_eq!(storage::BYTE,       0b_001);
+    assert_eq!(storage::WORD,       0b_010);
+    assert_eq!(storage::DWORD,      0b_011);
+    assert_eq!(storage::QWORD,      0b_100);
+    assert_eq!(storage::STRING,     0b_101);
+    assert_eq!(storage::BLOB,       0b_110);
+    assert_eq!(storage::CONTAINER,  0b_111);
 }
