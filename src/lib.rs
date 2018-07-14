63d99ebb3d7ed835f7baab71844e5984ab256124
Hai Bison <haibison.apps@gmail.com>
2018-07-14 15:24:12 +0700

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -15,18 +15,63 @@
 //! - All official types are supported.
 //! - User defined types are _not_ yet supported.
 //!
-//! # TODO: docs for beginners
-//!
 //! # Notes
 //!
 //! - `#![no_std]` _might_ be supported when [`alloc`][crate:alloc] crate is stabilized.
 //! - `IR` stands for _implementation in Rust_.
+//! - Core encoder and decoder are almost done (except decoder for user defined types). However API might change, as the project is still in
+//!   early development phase.
+//!
+//! # Quick examples
+//!
+//! Most functionalities are provided via 2 traits: [`Encoder`][trait:value::Encoder], [`Decoder`][trait:value::Decoder].
+//!
+//! This example demonstrates a simple file container:
+//!
+//! ```
+//! extern crate binn_ir;
+//!
+//! use std::collections::HashMap;
+//! use std::io::Cursor;
+//!
+//! use binn_ir::value::{Value, Encoder, Decoder};
+//!
+//! const MAGIC_NUMBER: u64 = 0xABCD;
+//!
+//! // Buffer
+//! let mut buf: Vec<u8> = vec![];
+//!
+//! // Magic number
+//! buf.encode_u64(MAGIC_NUMBER).unwrap();
+//!
+//! // A single file header contains: name and hash
+//! let file_header = {
+//!     let mut map = HashMap::new();
+//!     map.insert(String::from("name"), Value::from("sun"));
+//!     map.insert(String::from("hash"), Value::U64(0));
+//!     map
+//! };
+//! let file_content = "is hot";
+//! buf.encode_object(file_header.clone());
+//! buf.encode_blob(file_content.as_bytes());
+//!
+//! // Now read data back
+//! let mut cursor = Cursor::new(&buf);
+//! assert_eq!(cursor.decode_u64().unwrap(), MAGIC_NUMBER);
+//! assert_eq!(cursor.decode_object().unwrap(), file_header);
+//! assert_eq!(cursor.decode_blob().unwrap(), file_content.as_bytes());
+//! assert_eq!(cursor.position(), buf.len() as u64);
+//! ```
+//!
+//! ---
 //!
 //! _Special thanks to [Binn]'s authors for their hard work._
 //!
 //! [Semantic Versioning 2.0.0]: https://semver.org/spec/v2.0.0.html
 //! [Binn]: https://github.com/liteserver/binn
 //! [crate:alloc]: https://doc.rust-lang.org/alloc/
+//! [trait:value::Encoder]: value/trait.Encoder.html
+//! [trait:value::Decoder]: value/trait.Decoder.html
 
 // TODO: enable this flag when `alloc` crate is stabilized
 // #![no_std]
