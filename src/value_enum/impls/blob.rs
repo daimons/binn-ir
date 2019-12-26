3dfc4fdc234985844d2f3df9549a1c06ef64ed28
Hai Bison <haibison.apps@gmail.com>
2019-12-26 19:50:02 +0700

    Value: added new shortcut: as_mut_blob()

--- src/value_enum/impls/blob.rs
+++ src/value_enum/impls/blob.rs
@@ -3,7 +3,7 @@
 //! # Shortcuts for `Value::Blob`
 
 use {
-    crate::{Error, Result, Value},
+    crate::{Blob, Error, Result, Value},
 };
 
 /// # Shortcuts for [`Blob`](#variant.Blob)
@@ -13,6 +13,16 @@ impl Value {
     ///
     /// Returns an error if the value is not a blob.
     pub fn as_blob(&self) -> Result<&[u8]> {
+        match self {
+            Value::Blob(blob) => Ok(blob),
+            _ => Err(Error::from(__!("Value is not a Blob"))),
+        }
+    }
+
+    /// # If the value is a blob, returns a mutable reference of it
+    ///
+    /// Returns an error if the value is not a blob.
+    pub fn as_mut_blob(&mut self) -> Result<&mut Blob> {
         match self {
             Value::Blob(blob) => Ok(blob),
             _ => Err(Error::from(__!("Value is not a Blob"))),
