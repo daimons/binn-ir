684b8c313b795653402d8c2134c6bd6395c94abb
Hai Bison <haibison.apps@gmail.com>
2019-12-31 21:39:06 +0700

    >>> RELEASE: 0.14.2 <<<

--- src/version_info.rs
+++ src/version_info.rs
@@ -1,22 +1,12 @@
 // License: see LICENSE file at root directory of `master` branch
 
-//! # `0.14.1` _(December 29th, 2019)_
+//! # `0.14.2` _(December 31st, 2019)_
 //!
 //! ## Changes
 //!
-//! Added new functions:
+//! Implemented `From<Cow<'_, str>>` for [`Value`][::Value].
 //!
-//! ```ignore
-//! pub fn map_from<K, V>(key: K, value: V) -> Value
-//! where
-//!     K: Into<MapKey>,
-//!     V: Into<Value>;
-//!
-//! pub fn object_from<K, V>(key: K, value: V) -> Value
-//! where
-//!     K: Into<ObjectKey>,
-//!     V: Into<Value>;
-//! ```
+//! [::Value]: ../enum.Value.html
 //!
 //! <!-- 6c7646a9-b83c08ea-293526a9-07b9e036-771f8426-e7b616d7-b4c80325-7c945520-63b0d381-cc74fde9-9b3a28d4-6fa6c0d9-7473c4c2-ff18119d-3a308dd1-9452dfff -->
 //!
@@ -26,8 +16,12 @@
 //!
 //! ## Development dependencies
 //!
-//! No dependencies.
+//! | Name | Version | Notes
+//! | ---- | ------- | -----
+//! | [`kib`][crate:kib] | `^3` | **Required**
 //!
 //! ## Build dependencies
 //!
 //! No dependencies.
+//!
+//! [crate:kib]: https://bitbucket.org/haibison/kib
